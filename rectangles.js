c3757db6340dac591855608056fef05743b53032
Paul Smart <pdsmart6@gmail.com>
2021-12-15 23:19:15 +0000

    11 Tests Passed!

--- rectangles.js
+++ rectangles.js
@@ -1,31 +1,29 @@
 function count(input) {
     function transform(strArr){
     let outputArr = [];
+    let outputStr = ``;
     if (strArr === []){
-        return []
+        return ([],``)
     } else for (let i=0;i<strArr.length;i++){
         for (let j=0;j<strArr[i].length;j++){
         if (strArr[i].charAt(j) === '+'){
-            outputArr.push([j,i])
+            outputStr += `${j},${i}`;
+            outputArr.push([j,i]); 
         }
         }
-    } return outputArr;
+    } return [outputArr, outputStr]
     }
-    function parseCoordArray(coordArr) {
+    function parseCoords(coordArr,coordStr) {
     let rectangleCount = 0;
-    let hash = {};
-    for (let k=0;k<coordArr.length;k++){
-        hash[coordArr[k]] = k;
-    }
     for (let i=0;i<coordArr.length;i++){
     for (let j=0;j<coordArr.length;j++){
     if (coordArr[i][0] <= coordArr[j][0]){continue;}
-    if (coordArr[i][1] <= coordArr[i][1]){continue;}
-    if (hash.hasOwnProperty(coordArr[i][0],coordArr[j][1]])&&hash.hasOwnProperty([coordArr[j][0],coordArr[i][1]])){
-        rectangleeCount++;
+    if (coordArr[i][1] <= coordArr[j][1]){continue;}
+    if (coordStr.includes(`${coordArr[i][0]},${coordArr[j][1]}`)&&coordStr.includes(`${coordArr[j][0]},${coordArr[i][1]}`)){
+        rectangleCount++;
     }
     }
     }
     return rectangleCount;
-} return parseCoordArray(transform(input))
+} return parseCoords(transform(input)[0],transform(input)[1])
 }
