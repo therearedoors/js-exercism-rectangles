a7e6a70ac38b207d24ce8996b95c60e67dddd3d7
Paul Smart <91954372+therearedoors@users.noreply.github.com>
2021-12-16 11:23:44 +0000

    solution

--- rectangles.js
+++ rectangles.js
@@ -19,25 +19,28 @@ export function count(input) {
     let topRight = [];
     for (let i=0;i<coordArr.length;i++){
     for (let j=0;j<coordArr.length;j++){
-    if (coordArr[i][0] <= coordArr[j][0]){continue;}
-    if (coordArr[i][1] <= coordArr[j][1]){continue;}
+    if (coordArr[i][0] >= coordArr[j][0]){continue;}
+    if (coordArr[i][1] >= coordArr[j][1]){continue;}
     if (coordStr.includes(`${coordArr[i][0]},${coordArr[j][1]}`)&&coordStr.includes(`${coordArr[j][0]},${coordArr[i][1]}`)){
     botLeft = [coordArr[i][0],coordArr[j][1]];
     topRight = [coordArr[j][0],coordArr[i][1]];
     const checkAcross = (left,right) => {
-        let check = true;
+      let check = true
+      console.log(left);
         for (let k=left[0]+1;k<right[0];k++){
-        if (strArr[i].charAt(k) === "-") {continue}
+        if (strArr[left[1]].charAt(k) === '-' ||
+           strArr[left[1]].charAt(k) === '+') {continue}
           else {check = false}
        } return check
     }
     if (checkAcross(coordArr[i],topRight) &&
     checkAcross(botLeft,coordArr[j])){
     const checkDown = (top,bottom) => {
-        let check = true;
-        for (let l=i+1;l<coordArr[j][1];l++){
-        if (strArr[i][l] === "|") {continue}
-        else {check = false}
+      let check = true
+        for (let l=top[1]+1;l<bottom[1];l++){
+        if (strArr[l].charAt(top[0]) === '|' ||
+           strArr[l].charAt(top[0]) === '+') {continue}
+        else {check=false}
         } return check
         }
         if (checkDown(coordArr[i],botLeft) &&
